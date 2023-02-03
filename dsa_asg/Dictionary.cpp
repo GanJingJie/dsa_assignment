18ad1f0410d1c6054933aafc3c2ecceceedd41df
luketeran <s10222582@connect.np.edu.sg>
2023-02-03 23:57:24 +0800

    completed the dictionary class. but should rename to list. cause we not using dictionary. have yet to complete the implementation.

--- dsa_asg/Dictionary.cpp
+++ dsa_asg/Dictionary.cpp
@@ -4,135 +4,138 @@
 
 using namespace std;
 
-int charvalue(char c)
-{
-	if (isalpha(c))
-	{
-		if (isupper(c))
-			return (int)c - (int)'A';
-		else
-			return (int)c - (int)'a' + 26;
-	}
-	else
-		return -1;
-}
 
-Dictionary::Dictionary()
-{
-	for (int i = 0; i < max_size; i++)
-	{
-		items[i] = NULL;
-	}
-	size = 0;
-}
 
-Dictionary::~Dictionary() {
-	// Deconstructor required
-}
 
-int Dictionary::hash(KeyType key)
-{
-	int strvalue = NULL;
-	for (int i = 1; i < key.length(); i++)
-	{
-		strvalue += charvalue(key[i]);
-	}
-
-	return strvalue;
-}
-
-bool Dictionary::add(KeyType nKey, ItemType nUser)
-{
-	int value = hash(nKey);
-	if (items[value] == NULL)
-	{
-		Node* newNode = new Node();
-		newNode->key = nKey;
-		newNode->user = nUser;
-		newNode->post = NULL;
-		newNode->next = NULL;
-
-		items[value] = newNode;
-	}
-
-	else
-	{
-		Node* temp = items[value];
-		if (temp->key == nKey)
-		{
-			return false;
-		}
-
-		while (temp->next != NULL)
-		{
-			if (temp->key == nKey)
-			{
-				return false;
-			}
-
-			temp = temp->next;
-		}
-
-		Node* newNode = new Node();
-		newNode->key = nKey;
-		newNode->user = nUser;
-		newNode->post = NULL;
-		newNode->key = nKey;
-		items[value] = newNode;
-		temp->next = newNode;
-	}
-
-	size++;
-	cout << "Topic created successfully!" << endl;
-	return true;
-}
-
-void Dictionary::remove(KeyType key)
-{
-	int value = hash(key);
-	if (items[value] != NULL)
-	{
-		Node* temp = items[value];
-		items[value] = NULL;
-		delete temp;
-		size--;
-	}
-}
-
-postNode *Dictionary::getPost(KeyType key)
-{
-	int value = hash(key);
-	if (items[value] != NULL)
-	{
-		return items[value]->post;
-	}
-
-}
-
-bool Dictionary::isEmpty()
-{
-	if (size == 0)
-	{
-		return true;
-	}
-	else
-		return false;
-}
-
-int Dictionary::getLength()
-{
-	return size;
-}
-
-void Dictionary::printTopic()
-{
-	int count = 1;
-	for (int i = 0; i < max_size; i++)
-	{
-		if (items[i] != NULL)
-		{
-			cout << count << ". " << items[i]->key << endl;
-			count++;
-		}
-	}
-}
+//int charvalue(char c)
+//{
+//	if (isalpha(c))
+//	{
+//		if (isupper(c))
+//			return (int)c - (int)'A';
+//		else
+//			return (int)c - (int)'a' + 26;
+//	}
+//	else
+//		return -1;
+//}
+//
+//Dictionary::Dictionary()
+//{
+//	for (int i = 0; i < max_size; i++)
+//	{
+//		items[i] = NULL;
+//	}
+//	size = 0;
+//}
+//
+//Dictionary::~Dictionary() {
+//	// Deconstructor required
+//}
+//
+//int Dictionary::hash(KeyType key)
+//{
+//	int strvalue = NULL;
+//	for (int i = 1; i < key.length(); i++)
+//	{
+//		strvalue += charvalue(key[i]);
+//	}
+//
+//	return strvalue;
+//}
+//
+//bool Dictionary::add(KeyType nKey, ItemType nUser)
+//{
+//	int value = hash(nKey);
+//	if (items[value] == NULL)
+//	{
+//		Node* newNode = new Node();
+//		newNode->key = nKey;
+//		newNode->user = nUser;
+//		newNode->post = NULL;
+//		newNode->next = NULL;
+//
+//		items[value] = newNode;
+//	}
+//
+//	else
+//	{
+//		Node* temp = items[value];
+//		if (temp->key == nKey)
+//		{
+//			return false;
+//		}
+//
+//		while (temp->next != NULL)
+//		{
+//			if (temp->key == nKey)
+//			{
+//				return false;
+//			}
+//
+//			temp = temp->next;
+//		}
+//
+//		Node* newNode = new Node();
+//		newNode->key = nKey;
+//		newNode->user = nUser;
+//		newNode->post = NULL;
+//		newNode->key = nKey;
+//		items[value] = newNode;
+//		temp->next = newNode;
+//	}
+//
+//	size++;
+//	cout << "Topic created successfully!" << endl;
+//	return true;
+//}
+//
+//void Dictionary::remove(KeyType key)
+//{
+//	int value = hash(key);
+//	if (items[value] != NULL)
+//	{
+//		Node* temp = items[value];
+//		items[value] = NULL;
+//		delete temp;
+//		size--;
+//	}
+//}
+//
+//postNode *Dictionary::getPost(KeyType key)
+//{
+//	int value = hash(key);
+//	if (items[value] != NULL)
+//	{
+//		return items[value]->post;
+//	}
+//
+//}
+//
+//bool Dictionary::isEmpty()
+//{
+//	if (size == 0)
+//	{
+//		return true;
+//	}
+//	else
+//		return false;
+//}
+//
+//int Dictionary::getLength()
+//{
+//	return size;
+//}
+//
+//void Dictionary::printTopic()
+//{
+//	int count = 1;
+//	for (int i = 0; i < max_size; i++)
+//	{
+//		if (items[i] != NULL)
+//		{
+//			cout << count << ". " << items[i]->key << endl;
+//			count++;
+//		}
+//	}
+//}
--- dsa_asg/Dictionary.h
+++ dsa_asg/Dictionary.h
@@ -3,35 +3,58 @@
 #include "Posts.h"
 using namespace std;
 
-const int max_size = 1000;
 typedef string ItemType;
-typedef string KeyType;
 
-struct Node
-{
-	KeyType  key;
-	ItemType user;
-	postNode* post;
-	Node* next;
-};
 
 class Dictionary
 {
 private:
-	Node* items[max_size];
+
+	struct Node
+	{
+		ItemType topic;
+		ItemType user;
+		Posts* postNode;
+		Node* next;
+	};
+
+	Posts post;
+	Node* firstNode;
 	int  size;
 
 public:
 
+	//Constructor
 	Dictionary();
+	//Destructor
 	~Dictionary();
-
-	int hash(KeyType key);
-	bool add(KeyType newKey, ItemType user);
-	void remove(KeyType key);
-	postNode* getPost(KeyType key);
+	// add topic to dictionary
+	bool add(ItemType topic, ItemType user);
+	//add post to topic
+	bool addPost(ItemType post);
+	//add reply to post
+	bool addReply(ItemType reply);
+	//edit topic at index
+	bool edit(int index, ItemType topic, ItemType user);
+	//edit post at index
+	bool editPost(int index, ItemType post, ItemType user);
+	//edit reply at index.
+	bool editReply(int index, ItemType reply, ItemType user);
+	// remove topic from dictionary
+	void remove(int index);
+	// remove post from topic
+	void removePost(int index);
+	// remove reply from post
+	void removeReply(int index);
+	// print all topics
+	void printTopic();
+	// print all posts
+	void printPost();
+	// print all replies
+	void printReply();
+	// check if dictionary is empty
 	bool isEmpty();
+	// return int of dictionary length
 	int getLength();
 
-	void printTopic();
 };
